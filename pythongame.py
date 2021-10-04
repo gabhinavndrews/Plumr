ec321092da98f8f27f1298a6a07cea237f7fff60
Jordan Saleh <jsale7@unh.newhaven.edu>
2021-10-04 16:35:35 -0400

    window closes immediately needs fix

--- pythongame.py
+++ pythongame.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 # game window
 WIDTH, HEIGHT = 900, 500
@@ -6,14 +7,21 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Plumbr")
 
 WHITE = (255, 255, 255)
-# events
-def main():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
 
-        WIN.fill(WHITE)
-    
-    pygame.quit()
+def window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+
+def main(): # WINDOW CLOSES IMMEDIATELY 10/4/21 NEEDS FIX
+    running = True
+    try:
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT():
+                    run = False
+        pygame.quit()
+    except SystemExit:
+        pygame.quit()
+
+
