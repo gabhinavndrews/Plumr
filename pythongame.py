abb74ab2f8d8646d693e1a78d8dd50d6bc5258a8
Jordan Saleh <jsale7@unh.newhaven.edu>
2021-10-04 16:21:08 -0400

    10/4/21 window

--- pythongame.py
+++ pythongame.py
@@ -1,14 +1,19 @@
-# python game file
 import pygame
 
+# game window
 WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode(WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Plumbr")
 
+WHITE = (255, 255, 255)
+# events
 def main():
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    pygame.quit()
 
+        WIN.fill(WHITE)
+    
+    pygame.quit()
