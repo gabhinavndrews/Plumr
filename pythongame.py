027c0c5a975a861308c846062c9ec4f0e27d0e39
Jordan Saleh <jsale7@unh.newhaven.edu>
2021-10-04 16:55:11 -0400

    added favicon

--- pythongame.py
+++ pythongame.py
@@ -2,9 +2,11 @@ import pygame
 import sys
 
 pygame.init()   # initializing pygame
-sys.init() # initializing sys
 
 pygame.display.set_caption('Plumbr')    # creating title of game
+favicon = pygame.image.load('pipe.png') # favicon  
+pygame.display.set_icon(favicon)
+
 WIDTH, HEIGHT = 900, 500    # width: 900, height: 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))   # creating window
 
@@ -14,3 +16,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    window.fill((255,255,255))
+    pygame.display.update()
