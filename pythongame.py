2195f1ad3e994fec2c3f0fbce6313eb7d7f77322
Jordan Saleh <jsale7@unh.newhaven.edu>
2021-10-06 11:13:01 -0400

    setting up background and setting it

--- pythongame.py
+++ pythongame.py
@@ -10,12 +10,15 @@ pygame.display.set_icon(favicon)
 WIDTH, HEIGHT = 900, 500    # width: 900, height: 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))   # creating window
 
+background = pygame.image.load("plumbrback.png")
+
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     window.fill((255,255,255))
     pygame.display.update()
+
+    pygame.gameDisplay.blit(background, (0,0))
