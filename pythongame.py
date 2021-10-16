d981cf4f775af13006744833515e405640d87cd0
Jordan Saleh <jsale7@unh.newhaven.edu>
2021-10-16 13:02:35 -0400

    background added

--- pythongame.py
+++ pythongame.py
@@ -3,22 +3,25 @@ import sys
 
 pygame.init()   # initializing pygame
 
-pygame.display.set_caption('Plumbr')    # creating title of game
+pygame.display.set_caption('Plumr')    # creating title of game
 favicon = pygame.image.load('pipe.png') # favicon  
 pygame.display.set_icon(favicon)
+
+# background
+background = pygame.image.load('PlumrBG.png')
 
 WIDTH, HEIGHT = 900, 500    # width: 900, height: 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))   # creating window
 
-background = pygame.image.load("plumbrback.png")
+window.fill((0,0,0))
+window.blit(background,(0,0))
 
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
+
+
         if event.type == pygame.QUIT:
             running = False
-    window.fill((255,255,255))
     pygame.display.update()
-
-    pygame.gameDisplay.blit(background, (0,0))
